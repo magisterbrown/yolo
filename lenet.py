0d3a60cfceaddc18fb115812a8cf94b1f1d9070f
Artur <magisterbrownie@gmail.com>
2021-05-27 09:06:38 +0200

    startup

---
+++ lenet.py
@@ -0,0 +1,12 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class LeNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+        print("News")
+
+    def forward(self, x):
+        return x
+
