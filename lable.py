b5205556314f873b4b4c71eb1fa0585390db9cf2
Artur <magisterbrownie@gmail.com>
2021-07-13 11:20:38 +0200

    Lables done but not tested

--- lable.py
+++ lable.py
@@ -7,20 +7,16 @@ class Rectangle:
     def __init__(self, points: ET.Element, canvas: (int,int)):
         self.ow = canvas[0]
         self.oh = canvas[1]
+        self.category = points[0].text.strip()
         self.xmin = self.x_prop(float(points[4][0].text))
         self.ymin = self.y_prop(float(points[4][1].text))
         self.xmax = self.x_prop(float(points[4][2].text))
         self.ymax = self.y_prop(float(points[4][3].text))
+        self.cenx = (self.xmax+self.xmin)/2
+        self.ceny = (self.ymax+self.ymin)/2
         
         self.blocks = 1/7
         self.secarea = self.blocks*self.blocks
-
-    # def regions(self) -> ((int,int),(int,int)):
-    #     leftx = int(self.x_prop(self.xmin)/self.blocks)
-    #     topy = int(self.y_prop(self.ymin)/self.blocks)
-    #     rightx = int(self.x_prop(self.xmax)/self.blocks)
-    #     boty = int(self.y_prop(self.ymin)/self.blocks)
-    #     return ((leftx,rightx),(topy,boty))
 
     def x_prop(self, x):
         return x/self.ow
@@ -37,45 +33,58 @@ class Rectangle:
         return (top,bottom)
 
     def center(self) -> (int,int):
-        cx = (self.xmax+self.xmin)/2
-        cy = (self.ymax+self.ymin)/2
-        cx = int(cx/self.blocks)
-        cy = int(cy/self.blocks)
+        
+        cx = int(self.cenx/self.blocks)
+        cy = int(self.ceny/self.blocks)
 
         return (cx,cy)
-    # def iou(self, square: (int, int)):
-    #     xsecmin = square[0]*self.blocks
-    #     xsecmax = (square[0]+1)*self.blocks
-    #     ysecmin = square[1]*self.blocks
-    #     ysecmax = (square[1]+1)*self.blocks
-    #     width = max(0,min(xsecmax, self.x_prop(self.xmax)) - max(xsecmin, self.x_prop(self.xmin))) 
-    #     height = max(0,min(ysecmax, self.y_prop(self.ymax)) - max(ysecmin, self.y_prop(self.ymin))) 
-    #     intersection = width*height
-    #     box = (self.x_prop(self.xmax-self.xmin) * self.y_prop(self.ymax - self.ymin)) 
-    #     union = box+self.secarea-intersection
 
-    #     return intersection/union
+    def marigns(self) -> (float,float):
+        cell = self.center()
+        margx = self.cenx-cell[0]*self.blocks
+        margy = self.ceny-cell[1]*self.blocks
+
+        return (margx,margy)
+    
+    def size(self) -> (float,float):
+        width = self.xmax-self.xmin
+        heigth = self.ymax-self.ymin
+        return (width,heigth)
         
 class Lable:
-    def __init__(self, path: str):
+    def __init__(self, path: str, cats):
         tree = ET.parse(path)
         root =  tree.getroot()
-        self.root = root
         self.width = float(root[4][0].text)
         self.height = float(root[4][1].text)
         self.rectangles = list()
         self.prediction = torch.zeros([25,7,7], dtype=torch.float64)
+        self.cats = cats
 
         for el in root.iter('object'):
             self.rectangles.append(Rectangle(el,(self.width,self.height)))
 
-    def get_prediction(self) -> torch.Tensor:
+    # def get_prediction(self) -> torch.Tensor:
+    #     for rectangle in self.rectangles:
+    #         pass
+    #     return self.prediction
+
+    # def add_rectangle(self, rectangle: Rectangle):
+    #     print(rectangle.center())
+
+    def target(self,width: int,height: int) -> torch.Tensor:
         for rectangle in self.rectangles:
-            pass
+            pos = rectangle.center()
+            self.prediction[4,pos[1],pos[0]] = 1
+            cat = self.cats[rectangle.category]+5
+            self.prediction[cat,pos[1],pos[0]] = 1
+
+            margins = rectangle.margins()
+            size = rectangle.size()
+
+            self.prediction[0,pos[1],pos[0]] = margins[0]*width
+            self.prediction[1,pos[1],pos[0]] = margins[1]*height
+            self.prediction[2,pos[1],pos[0]] = size[0]*width
+            self.prediction[3,pos[1],pos[0]] = size[1]*height
+
         return self.prediction
-
-    def add_rectangle(self, rectangle: Rectangle):
-        print(rectangle.center())
-
-    def target(self):
-        pass
--- lableTest.py
+++ lableTest.py
@@ -4,13 +4,23 @@ from lable import Lable
 class TestLable(unittest.TestCase):
     
     name = '006104'
-    lable = Lable(f'VOC2007/Annotations/{name}.xml')
+    def setUp(self):
+        with open('VOC2007/categories.txt') as f:
+            cats = {}
+            for key,val in enumerate(f):
+                cats[val.strip()] = key
+            print(f'{cats}')
+        self.lable = Lable(f'VOC2007/Annotations/{self.name}.xml')
     def test_creation(self):
-        
         self.lable.add_rectangle(self.lable.rectangles[2])
     
     def fields(self):
-        print(self.lable.root.find("object")[0].text)
+        print(self.lable.rectangles[1].category)
+    
+    def target(self):
+        print(type(self.lable.target()))
+
+    
 
 if __name__ == '__main__':
     print("df")
