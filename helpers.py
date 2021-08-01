d7f9fa7a4c47e45b4469100c3b1498be058c7d4e
Artur <magisterbrownie@gmail.com>
2021-08-01 10:20:38 +0000

    Example image

--- helpers.py
+++ helpers.py
@@ -2,10 +2,10 @@ import torch
 import cv2
 import numpy as np
 from statistics import mean
-def draw(image: torch.Tensor,lable: torch.Tensor,pos: (int,int),cats: dict):
+def draw(image ,lable: torch.Tensor,pos: (int,int),cats: dict):
   box = lable[:,pos[0],pos[1]]
   parts = 1/lable.shape[-1]
-  side = image.shape[-1]
+  side = image.shape[0]
   cat = float(torch.argmax(box[5:]))
   vl = int(float(torch.max(box[5:]))*float(box[4])*100)
   
@@ -18,7 +18,7 @@ def draw(image: torch.Tensor,lable: torch.Tensor,pos: (int,int),cats: dict):
   y2 = int(centery+box[3]*side/2)
 
   color = np.random.rand(3,)
-  denorm = cv2.rectangle(denorm.copy() ,(x1,y1),(x2,y2),color,3)
+  denorm = cv2.rectangle(image ,(x1,y1),(x2,y2),color,3)
   denorm=cv2.putText(denorm,f'{cats[cat]} {vl}%', (x1,y1-5),cv2.FONT_HERSHEY_TRIPLEX, 1, color)
 
   return denorm
